63cb86d014657443c7bb1dedd50bdbc68656c471
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2021-12-05 00:05:50 +0300

    refactoring 3 (dict instead of switch)

--- jest/src/tdd_tasking.js
+++ jest/src/tdd_tasking.js
@@ -9,18 +9,26 @@ const parse = (item) => {
   }
 }
 
+const tasks = {
+  'd': {key: 'Dev days', value: 0.5},
+  'D': {key: 'Dev days', value: 1},
+  'q': {key: 'QA days', value: 0.5},
+  'Q': {key: 'QA days', value: 1},
+  'b': {key: 'Blocked days', value: 0.5},
+  'B': {key: 'Blocked days', value: 1}
+}
+
+const parseTaskByTrackingMark = (item) => tasks[item]
+
 const convert_tracking_marks_to_human_format = (tracking_marks) => {
   let state = {
     "Dev days": 0,
     "QA days": 0,
     "Blocked days": 0
   }
-  // [...tracking_marks]
-  // tracking_marks.split('')
-  Array.from(tracking_marks)
-  // Object.assign([], tracking_marks)
-  .map(item => {
-    const {key, value} = parse(item)
+  // [...tracking_marks] // tracking_marks.split('') // Object.assign([], tracking_marks)
+  Array.from(tracking_marks).map(item => {
+    let {key, value} = parseTaskByTrackingMark(item) // parse(item)
     state[key] += value
   })
   return state

280608d97e3ecba76641fd1469cf650a22787089
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2021-12-04 22:58:18 +0300

    switch instead of if

--- jest/src/tdd_tasking.js
+++ jest/src/tdd_tasking.js
@@ -1,12 +1,18 @@
 function convert_tracking_marks_to_human_format(tracking_marks) {
   let dev_days = 0, qa_days = 0, blocked_days = 0
-  tracking_marks.split('').map(item => {
-    if (item == 'd') dev_days += 0.5
-    if (item == 'D') dev_days += 1
-    if (item == 'q') qa_days += 0.5
-    if (item == 'Q') qa_days += 1
-    if (item == 'b') blocked_days += 0.5
-    if (item == 'B') blocked_days += 1
+  // [...tracking_marks]
+  tracking_marks.split('')
+  // Array.from(tracking_marks)
+  // Object.assign([], tracking_marks)
+  .map(item => {
+    switch(item) {
+      case 'd': return dev_days += 0.5
+      case 'D': return dev_days += 1
+      case 'q': return qa_days += 0.5
+      case 'Q': return qa_days += 1
+      case 'b': return blocked_days += 0.5
+      case 'B': return blocked_days += 1
+    }
   })
   return { "Dev days": dev_days, "QA days": qa_days, "Blocked days": blocked_days}
 }

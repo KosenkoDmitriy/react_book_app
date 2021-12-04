7abf884016f12438b10476af44dd10c9ef65a394
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2021-12-04 12:03:20 +0300

    calc functions: add, subsctract, multiply, divide

--- jest/src/calc.js
+++ jest/src/calc.js
@@ -1,5 +1,14 @@
 function add(x, y) {
-  return x + y;
+  return x + y
+}
+substract = (x, y) => {
+  return x - y
+}
+multiply = (x, y) => {
+  return x * y
+}
+divide = (x, y) => {
+  return x / y
 }
 
 module.exports = add
--- jest/src/calc.test.js
+++ jest/src/calc.test.js
@@ -1,6 +1,44 @@
-var add = require('./calc.js')
+require('./calc.js')
+
 describe('calculator', function() {
-  it('add two numbers', function() {
-    expect(add(1, 2)).toEqual(3)
+
+  describe('should perform addition', () => {
+    it('adds two positive numbers', () => {
+      expect(add(1, 2)).toEqual(3)
+    })
+    it('adds two negative numbers', () => {
+      expect(add(-1, -2)).toEqual(-3)
+    })
+    it('adds one negative and one positive numbers', () => {
+      expect(add(-1, 1)).toEqual(0)
+    })
+  })
+
+  describe('should perform substraction', () => {
+    it('subsctracts two positive numbers', () => {
+      expect(substract(1, 1)).toEqual(0)
+    })
+  })
+
+  describe('should perform multiplication', () => {
+    it("multiply two positive numbers", () => {
+      expect(multiply(1, 1)).toEqual(1)
+    })
+    it("multiply two negative numbers", () => {
+      expect(multiply(-1, -1)).toEqual(1)
+    })
+  })
+
+  describe('should perform division', () => {
+    it("divide two positive numbers", () => {
+      expect(divide(1, 1)).toEqual(1)
+      expect(divide(10, 5)).toEqual(2)
+    })
+    it("divide positive number on zero", () => {
+      expect(divide(1, 0)).toEqual(Infinity)
+    })
+    it("divide zero on number", () => {
+      expect(divide(0, 1)).toEqual(0)
+    })
   })
 })
